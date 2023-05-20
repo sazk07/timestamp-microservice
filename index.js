f5496659558c5b6cf008043bb53ca74c7d4cc0e3
shahan <shahan@gmail.com>
2023-05-20 22:17:44 +0500

    feat: return date in unix and UTCString formats in JSON

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const thisdate = new Date()
+const utcVar = thisdate.toUTCString()
+const unixVar = Date.now()
+console.log(unixVar)
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send({
+    unix: unixVar,
+    utc: utcVar
+  })
 })
 
 app.listen(port, () => {
