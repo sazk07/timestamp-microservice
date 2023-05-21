0d8986bde05b4fc4819bba350c441a6c65e83d8b
shahan <shahan@gmail.com>
2023-05-21 18:36:46 +0500

    fix: for correct parsing dates by new Date(date_string)

--- index.js
+++ index.js
@@ -3,43 +3,27 @@ const app = express()
 const port = 3000
 
 app.get("/api/:date_string?", (req, res) => {
-  let dateObj
-  let unixVal
-  let utcVal
-  let resObj
   const { date_string } = req.params
-  // check if string is date using regex tests
-  const dateRegEx = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
-  const isValidDate = dateRegEx.test(date_string)
-  const unixRegEx = /^[0-9]+$/;
-  const isValidUnixDate = unixRegEx.test(date_string)
-
-  try {
-    dateObj = new Date(date_string)
-    // is it in yyyy-mm-dd format
-    if (isValidDate) {
-      unixVal = dateObj.valueOf()
-    } else if (isValidUnixDate) {
-      // is it in all numbers format
-      unixVal = Number(date_string)
-      dateObj = new Date(unixVal)
-    } else if (!date_string) {
-      unixVal = Date.now()
-      dateObj = new Date(unixVal)
-    } else {
-      throw new Error
-    }
-    utcVal = dateObj.toUTCString()
-    resObj = {
-      unix:unixVal,
-      utc:utcVal
-    }
-  } catch (err) {
-    resObj = {
-      error: "Invalid Date"
-    }
+  let dateObj
+  if (!date_string) {
+    dateObj = new Date()
+  } else {
+    const checkUnix = date_string * 1
+    dateObj = isNaN(checkUnix) ? new Date(date_string) : new Date(checkUnix);
   }
-  res.json(resObj)
+  // check if valid format
+  if (dateObj == "Invalid Date") {
+    res.json({
+      error: "Invalid Date"
+    })
+  } else {
+    const unix = dateObj.getTime()
+    const utc = dateObj.toUTCString()
+    res.json({
+      unix,
+      utc
+    })
+  }
 })
 
 app.listen(port, () => {
