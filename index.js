f5ead618477213a619e13098b908bd3551c60461
shahan <shahan@gmail.com>
2023-05-21 11:13:20 +0500

    fix: date object and correct key value pairs and correct error object
    feat: test for unix style date
    
    fix: test if api is yyyy-mm-dd or number

--- index.js
+++ index.js
@@ -2,23 +2,44 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-const thisdate = new Date()
-const utcVar = thisdate.toUTCString()
-const unixVar = Date.now()
-console.log(unixVar)
+app.get("/api/:date_string?", (req, res) => {
+  let dateObj
+  let unixVal
+  let utcVal
+  let resObj
+  const { date_string } = req.params
+  // check if string is date using regex tests
+  const dateRegEx = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+  const isValidDate = dateRegEx.test(date_string)
+  const unixRegEx = /^[0-9]+$/;
+  const isValidUnixDate = unixRegEx.test(date_string)
 
-app.get("/api/2023-05-20", (req, res) => {
-  res.send({
-    unix: unixVar,
-    utc: utcVar
-  })
-})
-
-app.get("/api/1684603518875", (req, res) => {
-  res.send({
-    unix: unixVar,
-    utc: utcVar
-  })
+  try {
+    dateObj = new Date(date_string)
+    // is it in yyyy-mm-dd format
+    if (isValidDate) {
+      unixVal = dateObj.valueOf()
+    } else if (isValidUnixDate) {
+      // is it in all numbers format
+      unixVal = Number(date_string)
+      dateObj = new Date(unixVal)
+    } else if (!date_string) {
+      unixVal = Date.now()
+      dateObj = new Date(unixVal)
+    } else {
+      throw new Error
+    }
+    utcVal = dateObj.toUTCString()
+    resObj = {
+      unix:unixVal,
+      utc:utcVal
+    }
+  } catch (err) {
+    resObj = {
+      error: "Invalid Date"
+    }
+  }
+  res.json(resObj)
 })
 
 app.listen(port, () => {
