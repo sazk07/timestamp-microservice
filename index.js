9fe1e9b0131d3af2b501411a5b6d9187221b6419
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-19 00:05:11 +0500

    minor updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const express = require("express")
 const app = express()
 const port = 3000
@@ -5,18 +6,21 @@ const port = 3000
 app.get("/api/:date_string?", (req, res) => {
   const { date_string } = req.params
   let dateObj
+  // if empty
   if (!date_string) {
     dateObj = new Date()
   } else {
-    const checkUnix = date_string * 1
+    // check if number
+    const checkUnix = Number(date_string)
     dateObj = isNaN(checkUnix) ? new Date(date_string) : new Date(checkUnix);
   }
   // check if valid format
-  if (dateObj == "Invalid Date") {
+  if (dateObj.toString() === "Invalid Date") {
     res.json({
       error: "Invalid Date"
     })
   } else {
+    // get unix and utc times
     const unix = dateObj.getTime()
     const utc = dateObj.toUTCString()
     res.json({
